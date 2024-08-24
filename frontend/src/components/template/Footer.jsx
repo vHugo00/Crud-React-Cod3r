c5ab6b6b3e8b015854279edbd928f7c1716b3617
unknown <Vitor Hugo>
2024-08-24 13:40:39 -0300

    Projeto Finalizado

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+import React from 'react'
+
+const footer = props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido por <strong>Vitor Hugo</strong>
+        </span>
+    </footer>
+
+export default footer
